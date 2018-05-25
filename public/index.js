939502d468edad8f2fc8cc505714c264afd1ecc4
Gavin Jefferies <gavin@runtime.io>
2018-05-25 16:24:37 -0700

    loading, units & 24 hours

--- public/index.js
+++ public/index.js
@@ -13,10 +13,47 @@
     }
   }
 
+  function got24Hours() {
+    var min;
+    var avg;
+    var max;
+    try {
+      var payload = this.responseText;
+      payload = JSON.parse(payload);
+      var sum = 0;
+      payload.data.forEach(function(datum) {
+        var value = parseFloat(datum.v);
+        if (value) {
+          sum = sum + value;
+          if (!min || value < min) {
+            min = value;
+          }
+          if (!max || value > max) {
+            max = value;
+          }
+        }
+      });
+      if (sum !== 0) {
+        avg = sum / payload.data.length;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    if (min) {
+      updateTemp("24-hours-min", min.toFixed(1));
+    }
+    if (avg) {
+      updateTemp("24-hours-avg", avg.toFixed(1));
+    }
+    if (max) {
+      updateTemp("24-hours-max", max.toFixed(1));
+    }
+  }
+
   function updateTemp(id, value) {
     var element = document.getElementById(id);
-    if (element) {
-      element.innerHTML = value;
+    if (element && element.children[0]) {
+      element.children[0].innerHTML = value;
     }
   }
 
@@ -27,4 +64,12 @@
     "https://tidesandcurrents.noaa.gov/api/datagetter?station=9414290&date=latest&product=water_temperature&format=json&units=english&time_zone=lst_ldt"
   );
   getCurrentTemp.send();
+
+  var get24Hours = new XMLHttpRequest();
+  get24Hours.addEventListener("load", got24Hours);
+  get24Hours.open(
+    "GET",
+    "https://tidesandcurrents.noaa.gov/api/datagetter?station=9414290&range=24&product=water_temperature&format=json&units=english&time_zone=lst_ldt"
+  );
+  get24Hours.send();
 })();
