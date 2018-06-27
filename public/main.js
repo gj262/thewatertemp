921f855f8b46a4bdbc4c0161d7b22c83900f4f2e
Gavin Jefferies <gavin@runtime.io>
2018-06-27 12:27:13 -0700

    experiment with PWA

--- public/main.js
+++ public/main.js
@@ -40,3 +40,15 @@
     View.Comparison("comparison", selectedComparison, displayUnits);
   });
 })();
+
+if ("serviceWorker" in navigator) {
+  console.log("Will the service worker register?");
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function() {
+      console.log("Yes, it did.");
+    })
+    .catch(function(err) {
+      console.log("No it didn't. This happened: ", err);
+    });
+}
